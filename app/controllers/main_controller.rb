29f5802ef201216d189e9e0ccf56042747e93903
Mateusz Bielawski <mateusz.bielawski@long.int.pl>
2017-05-21 18:28:03 +0200

    Added Angular, selecting directories and files, double click on a directory opens it, possibility to return to the previous directories

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,11 @@
 class MainController < ApplicationController
+  layout "drive"
   def drive
-    render template: 'layouts/drive'
+    @root_directory =  Directory.find(User.find(user_id).root_directory_id)
   end
+
+  private
+    def user_id
+      User.maximum(:id)
+    end
 end
