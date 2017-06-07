d02a51b2c915d34033f5253edd14b7207a22c895
Mateusz Bielawski <mateusz.bielawski@long.int.pl>
2017-06-07 14:32:35 +0200

    one

--- app/uploaders/user_file_uploader.rb
+++ app/uploaders/user_file_uploader.rb
@@ -40,6 +40,9 @@ class UserFileUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
 
+  def content_type_blacklist
+    []
+  end
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
